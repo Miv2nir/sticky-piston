7eee2011a6584d53173becc98a4c370d1a75f053
miv2nir <miv2nir@gmail.com>
2025-12-23 14:09:11 +0300

    add shell shortcut for debug n such

---
+++ shell.py
@@ -0,0 +1,8 @@
+# Source - https://stackoverflow.com/a
+# Posted by rgtk, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-12-23, License - CC BY-SA 3.0
+
+from IPython import embed
+from stickypiston import *
+
+embed()
